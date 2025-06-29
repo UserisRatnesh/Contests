2a393605ab8566a85ff40e3cf9174ca99af381bf
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-06-29 14:51:49 +0530

    Weekly 455.

---
+++ leetcode/weekly_455/coin_change_II.cpp
@@ -0,0 +1,150 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+int helper(int index, vector<int> &coins, int amount, vector<vector<int>> &dp) {
+  if (amount == 0) {
+    return 1;
+  }
+
+  if (index >= coins.size() || amount < 0) {
+    return 0;
+  }
+
+  if (dp[index][amount] != -1) {
+    return dp[index][amount];
+  }
+
+  int pick_not_move = helper(index, coins, amount - coins[index], dp);
+  int only_move = helper(index + 1, coins, amount, dp);
+
+  dp[index][amount] = pick_not_move + only_move;
+  return dp[index][amount];
+}
+
+int change(int amount, vector<int> &coins) {
+  vector<vector<int>> dp(coins.size(), vector<int>(amount + 1, -1));
+  return helper(0, coins, amount, dp);
+}
+
+// Better approach
+int change_better(int amount, vector<int> &coins) {
+
+  // unsigned long long can be used since amount can never be negative
+  vector<unsigned long long> dp(amount + 1, 0);
+  dp[0] = 1;
+
+  for (auto coin : coins) {
+    for (int a = coin; a <= amount; a++) {
+      dp[a] += dp[a - coin];
+    }
+  }
+
+  return dp[amount];
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  fastio();
+}
