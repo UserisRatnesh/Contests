fa1e0a7104891e1ab432cbd31da7f0e78f2cbd6b
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-07-25 18:10:04 +0530

    Educational round 180 div 2.

---
+++ codeforces/educational_round_180_div_2/c.cpp
@@ -0,0 +1,174 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// OPTIMAL:
+void solve() {
+  ll n;
+  cin >> n;
+  vector<ll> vec(n);
+  for (auto &it : vec) {
+    cin >> it;
+  }
+
+  ll ans = 0;
+
+  for (ll i = 2; i < n; i++) {
+
+    ll limit = max(2 * vec[i], vec[n - 1]);
+    ll target = limit - vec[i];
+
+    ll l = 0, r = i - 1;
+    while (l < r) {
+      if (vec[l] + vec[r] > target) {
+        ans += (r - l);
+        r--;
+      } else {
+        l++;
+      }
+    }
+  }
+  cout << ans << nl;
+}
+
+void solveSelf() {
+  ll n;
+  cin >> n;
+  vector<ll> vec(n);
+
+  ll maxi = LLONG_MIN;
+  for (auto &it : vec) {
+    cin >> it;
+    maxi = max(maxi, it);
+  }
+
+  ll ans = 0;
+  sort(all(vec));
+
+  for (ll i = 0; i < n; i++) {
+    for (ll j = i + 1; j < n; j++) {
+
+      ll currMaxi = max(maxi, 2 * vec[j]);
+      ll req = currMaxi - (vec[i] + vec[j]);
+
+      ll uppBound = upper_bound(vec.begin() + i + 1, vec.begin() + j, req) -
+                    (vec.begin() + i + 1);
+
+      ll len = j - (i + 1);
+      ans += (len - uppBound);
+    }
+  }
+
+  cout << ans << nl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  // freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  fastio();
+  ll t;
+  cin >> t;
+  while (t--) {
+    solveSelf();
+  }
+}
