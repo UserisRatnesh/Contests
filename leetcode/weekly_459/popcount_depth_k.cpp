9fe807856d8b0b41ee86e0a93e74d8156fc71853
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-08-04 13:05:35 +0530

    Weekly 459.

---
+++ leetcode/weekly_459/popcount_depth_k.cpp
@@ -0,0 +1,157 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// TC: TLE
+int findDepth(long long val) {
+  int cnt = 0;
+  while (val > 1) {
+    val = set_bits(val);
+    cnt++;
+    if (cnt > 5) {
+      return -1;
+    }
+  }
+  return cnt;
+}
+
+vector<int> popcountDepth(vector<long long> &nums,
+                          vector<vector<long long>> &queries) {
+
+  int n = nums.size();
+  vector<int> ans;
+  vector<int> depthVec(n, 0);
+  for (int i = 0; i < n; i++) {
+    depthVec[i] = findDepth(nums[i]);
+  }
+
+  for (auto it : queries) {
+    int type = it[0];
+    if (type == 2) {
+      int index = it[1];
+      long long val = it[2];
+      nums[index] = val;
+      int depth = findDepth(val);
+      depthVec[index] = depth;
+    } else {
+      int l = it[1];
+      int r = it[2];
+      int k = it[3];
+
+      // l se r tak me kitna nums ka value hai jiske liye depth == k hai
+      // count number of elements == k from l to r in depthVec
+      int cnt = count(depthVec.begin() + l, depthVec.begin() + r + 1, k);
+      ans.push_back(cnt);
+    }
+  }
+
+  return ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+}
